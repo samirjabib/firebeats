7a86f62816e31dfec6f6dff8578345341f02ddf4
Samir Jabib <samir.jc123@gmail.com>
2023-07-11 13:39:44 -0500

    start working on api revalidate shopify

---
+++ src/app/api/revalite/route.ts
@@ -0,0 +1,10 @@
+import { TAGS } from "@/lib/shopify/constants";
+import { revalidateTag } from "next/cache";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
+}
