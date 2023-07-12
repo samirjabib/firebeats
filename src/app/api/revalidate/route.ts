2f51f4915fee00dced5b73216b524309c7ec0543
Samir Jabib <samir.jc123@gmail.com>
2023-07-11 23:14:40 -0500

    Protec routes

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -7,5 +7,9 @@ export const runtime = "edge";
 
 export async function POST(req: NextRequest): Promise<Response> {
   console.log("run");
+
+  const topic = headers().get("x-shopify-topic") || "unknown";
+  console.log(topic);
+
   return NextResponse.json({ status: 200, revalidated: true, now: Date.now() });
 }
